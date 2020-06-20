aa9b0ac0329b3dcfa466f31814ed57a1f369c6b3
xmanu2015 <xmanu2015@gmail.com>
2020-06-20 18:04:39 -0500

    Creacion de Proyecto - parte 2

--- app/src/main/java/com/valdemar/emprendedores/view/CategoriasFragment.java
+++ app/src/main/java/com/valdemar/emprendedores/view/CategoriasFragment.java
@@ -1,4 +1,4 @@
-package com.valdemar.emprendedores;
+package com.valdemar.emprendedores.view;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.valdemar.emprendedores.R;
 
 /**
  * A simple {@link Fragment} subclass.
