d17053156c594efbe0c991888094af73e1de0726
xmanu2015 <xmanu2015@gmail.com>
2020-06-27 18:46:58 -0500

    Validacion de campos en Creación de Proyecto.

--- app/src/main/java/com/valdemar/emprendedores/view/ui/proyectos/registro/CrearProyectoFragment.java
+++ app/src/main/java/com/valdemar/emprendedores/view/ui/proyectos/registro/CrearProyectoFragment.java
@@ -23,6 +23,7 @@ import android.widget.LinearLayout;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
@@ -80,11 +81,11 @@ public class CrearProyectoFragment extends Fragment {
 
     private ProgressDialog mProgresDialog;
     private StorageReference mStorage;
-    private  DatabaseReference mDatabase;
+    private DatabaseReference mDatabase;
     private Task<Uri> mRuta;
+    private boolean mFotoVideoSubido;
 
     private Uri mImageUri = null;
-
 
 
     public CrearProyectoFragment() {
@@ -144,7 +145,7 @@ public class CrearProyectoFragment extends Fragment {
         mEdtDescripcionSocio4 = (EditText) view.findViewById(R.id.edt_descripcion_socio4);
         mEdtSocio4 = (EditText) view.findViewById(R.id.edt_socio4);
         mEdtDescripcionSocio5 = (EditText) view.findViewById(R.id.edt_descripcion_socio5);
-        ImageButton btnSubirFotoVideo = (ImageButton) view.findViewById(R.id.btn_subir_foto_video) ;
+        ImageButton btnSubirFotoVideo = (ImageButton) view.findViewById(R.id.btn_subir_foto_video);
         mImgFoto = (ImageView) view.findViewById(R.id.img_foto_proyecto);
         btnSubirFotoVideo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -195,17 +196,23 @@ public class CrearProyectoFragment extends Fragment {
         });
 
 
-
     }
 
     public void agregarSocio() {
-        if (nroSociosActivos < maxSociosActivos){
+        if (nroSociosActivos < maxSociosActivos) {
             nroSociosActivos++;
-            switch (nroSociosActivos){
-                case 2: mLLSocio2.setVisibility(View.VISIBLE); break;
-                case 3: mLLSocio3.setVisibility(View.VISIBLE); break;
-                case 4: mLLSocio4.setVisibility(View.VISIBLE); break;
-                case 5: mLLSocio5.setVisibility(View.VISIBLE);
+            switch (nroSociosActivos) {
+                case 2:
+                    mLLSocio2.setVisibility(View.VISIBLE);
+                    break;
+                case 3:
+                    mLLSocio3.setVisibility(View.VISIBLE);
+                    break;
+                case 4:
+                    mLLSocio4.setVisibility(View.VISIBLE);
+                    break;
+                case 5:
+                    mLLSocio5.setVisibility(View.VISIBLE);
             }
         }
     }
@@ -230,11 +237,11 @@ public class CrearProyectoFragment extends Fragment {
     }
 
     private void abrirGaleria(String opcion) {
-        switch (opcion){
+        switch (opcion) {
             case "F":
                 Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 galleryIntent.setType("image/*");
-                startActivityForResult(galleryIntent,GALLERY_REQUEST);
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
                 break;
             case "V":
 
@@ -244,58 +251,63 @@ public class CrearProyectoFragment extends Fragment {
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
-             mImageUri = data.getData();
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+            mImageUri = data.getData();
 
             //mPostImageSelect.setImageURI(mImageUri);
             Glide.with(getActivity().getApplicationContext())
                     .load(mImageUri)
                     .into(mImgFoto);
 
+            mFotoVideoSubido = true;
         }
     }
 
     private void registrarProyecto(View v) {
-        final Proyecto proyecto = initDataProyecto();
-        mStorage = FirebaseStorage.getInstance().getReference();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Proyectos");
-        mProgresDialog= new ProgressDialog(getActivity());
+        if (validarCampos()) {
+            final Proyecto proyecto = initDataProyecto();
+            mStorage = FirebaseStorage.getInstance().getReference();
+            mDatabase = FirebaseDatabase.getInstance().getReference().child("Proyectos");
+            mProgresDialog = new ProgressDialog(getActivity());
 
-        if( mImageUri !=null){
-            mProgresDialog.setMessage("Publicando Proyectos");
-            mProgresDialog.setCancelable(false);
-            mProgresDialog.show();
-            final StorageReference filepath = mStorage
-                    .child("Proyectos_images")
-                    .child(mImageUri.getLastPathSegment());
-            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
+            if (mImageUri != null) {
+                mProgresDialog.setMessage("Publicando Proyectos");
+                mProgresDialog.setCancelable(false);
+                mProgresDialog.show();
+                final StorageReference filepath = mStorage
+                        .child("Proyectos_images")
+                        .child(mImageUri.getLastPathSegment());
+                filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(final UploadTask.TaskSnapshot taskSnapshot) {
 
-                    //filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                       @Override
-                       public void onSuccess(Uri uri) {
-                           Uri downloadUrl = uri;
+                        //filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                Uri downloadUrl = uri;
 
-                           mDatabase = FirebaseDatabase.getInstance().getReference().child("Proyectos");
+                                mDatabase = FirebaseDatabase.getInstance().getReference().child("Proyectos");
 
-                           DatabaseReference newPost = mDatabase.push();
+                                DatabaseReference newPost = mDatabase.push();
 
-                           proyecto.setImagen(downloadUrl.toString());
+                                proyecto.setImagen(downloadUrl.toString());
 
-                           newPost.setValue(proyecto);
+                                newPost.setValue(proyecto);
 
-                           //Timestamp fechaRegistro = getFecha();
-                           //newPost.child("fechaRegistro").setValue(fechaRegistro);
-                           mProgresDialog.dismiss();
-                       }
-                    });
+                                //Timestamp fechaRegistro = getFecha();
+                                //newPost.child("fechaRegistro").setValue(fechaRegistro);
+                                mProgresDialog.dismiss();
+                            }
+                        });
 
+                    }
 
-                }
+                });
+            }
 
-            });
+        } else {
+
         }
 
 
@@ -304,26 +316,25 @@ public class CrearProyectoFragment extends Fragment {
     private Proyecto initDataProyecto() {
         Proyecto proyecto = new Proyecto();
 
-
-            proyecto.setCategoria(mCategoria.getNombre());
-            proyecto.setNombre(mEdtNombreProyecto.getText().toString());
-            proyecto.setDescripcion(mEdtDescripcionProyecto.getText().toString());
-            proyecto.setSocio1(mEdtSocio1.getText().toString());
-            proyecto.setDescripcionSocio1(mEdtDescripcionSocio1.getText().toString());
-            proyecto.setSocio2(validarEditText(mEdtSocio2) ? mEdtSocio2.getText().toString() : "");
-            proyecto.setDescripcionSocio2(validarEditText(mEdtDescripcionSocio2) ? mEdtDescripcionSocio2.getText().toString() : "");
-            proyecto.setSocio4(validarEditText(mEdtSocio3) ? mEdtSocio3.getText().toString() : "");
-            proyecto.setDescripcionSocio3(validarEditText(mEdtDescripcionSocio3) ? mEdtDescripcionSocio3.getText().toString() : "");
-            proyecto.setSocio4(validarEditText(mEdtSocio4) ? mEdtSocio4.getText().toString() : "");
-            proyecto.setDescripcionSocio4(validarEditText(mEdtDescripcionSocio4) ? mEdtDescripcionSocio4.getText().toString() : "");
-            proyecto.setSocio5(validarEditText(mEdtSocio5) ? mEdtSocio5.getText().toString() : "");
-            proyecto.setDescripcionSocio5(validarEditText(mEdtDescripcionSocio5) ? mEdtDescripcionSocio5.getText().toString() : "");
+        proyecto.setCategoria(mCategoria.getNombre());
+        proyecto.setNombre(mEdtNombreProyecto.getText().toString());
+        proyecto.setDescripcion(mEdtDescripcionProyecto.getText().toString());
+        proyecto.setSocio1(mEdtSocio1.getText().toString());
+        proyecto.setDescripcionSocio1(mEdtDescripcionSocio1.getText().toString());
+        proyecto.setSocio2(validarEditText(mEdtSocio2) ? mEdtSocio2.getText().toString() : "");
+        proyecto.setDescripcionSocio2(validarEditText(mEdtDescripcionSocio2) ? mEdtDescripcionSocio2.getText().toString() : "");
+        proyecto.setSocio4(validarEditText(mEdtSocio3) ? mEdtSocio3.getText().toString() : "");
+        proyecto.setDescripcionSocio3(validarEditText(mEdtDescripcionSocio3) ? mEdtDescripcionSocio3.getText().toString() : "");
+        proyecto.setSocio4(validarEditText(mEdtSocio4) ? mEdtSocio4.getText().toString() : "");
+        proyecto.setDescripcionSocio4(validarEditText(mEdtDescripcionSocio4) ? mEdtDescripcionSocio4.getText().toString() : "");
+        proyecto.setSocio5(validarEditText(mEdtSocio5) ? mEdtSocio5.getText().toString() : "");
+        proyecto.setDescripcionSocio5(validarEditText(mEdtDescripcionSocio5) ? mEdtDescripcionSocio5.getText().toString() : "");
 
 
         return proyecto;
     }
 
-    public Timestamp getFecha(){
+    public Timestamp getFecha() {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
         return timestamp;
@@ -331,10 +342,35 @@ public class CrearProyectoFragment extends Fragment {
 
 
     private boolean validarCampos() {
+
+        if (!(validarEditText(mEdtNombreProyecto)
+                && validarEditText(mEdtDescripcionProyecto)
+                && validarEditText(mEdtSocio1) && validarEditText(mEdtDescripcionSocio1)
+                && mFotoVideoSubido)) {
+            showSnackBar("Campos incompletos y/o falta subir foto o video");
+            return false;
+        }
+
+
+        if (!((validarEditText(mEdtSocio2) && validarEditText(mEdtDescripcionSocio2))
+                || (validarEditText(mEdtSocio3) && validarEditText(mEdtDescripcionSocio3))
+                || (validarEditText(mEdtSocio4) && validarEditText(mEdtDescripcionSocio4))
+                || (validarEditText(mEdtSocio5) && validarEditText(mEdtDescripcionSocio5)))) {
+            showSnackBar("Si se agrega un socio se debe agregar su descripción");
+            return false;
+        }
+
+
         return true;
     }
 
-    private boolean validarEditText(EditText edt){
-        return (edt!=null && !edt.getText().toString().isEmpty()) ? true : false;
+    private boolean validarEditText(EditText edt) {
+        return (edt != null && !edt.getText().toString().isEmpty()) ? true : false;
+    }
+
+    public void showSnackBar(String msg) {
+        Snackbar
+                .make(getActivity().findViewById(R.id.constraint_crear_proyecto), msg, Snackbar.LENGTH_SHORT)
+                .show();
     }
 }
