c877fe5961a2ba9292ea3b2f6ff08bbede1a1cfc
xmanu2015 <xmanu2015@gmail.com>
2020-11-14 19:29:30 -0500

    Reacciones - carga en la lista de proyectos y reaccionar sólo una a la vez.

--- app/src/main/java/com/valdemar/emprendedores/view/ui/proyectos/lista/buscador/SearchPlaceAdapter.java
+++ app/src/main/java/com/valdemar/emprendedores/view/ui/proyectos/lista/buscador/SearchPlaceAdapter.java
@@ -12,16 +12,19 @@ import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.github.pgreze.reactions.ReactionPopup;
 import com.github.pgreze.reactions.ReactionsConfigBuilder;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.valdemar.emprendedores.R;
-import com.valdemar.emprendedores.view.ui.proyectos.lista.Category;
 import com.valdemar.emprendedores.view.ui.proyectos.lista.ItemFeed;
 
 import java.util.ArrayList;
@@ -35,7 +38,11 @@ public class SearchPlaceAdapter extends RecyclerView.Adapter<SearchPlaceAdapter.
     public ArrayList<ItemFeed> arrayList;
     public ArrayList<ItemFeed> arrayListFiltered;
     private IModal listener;
-    private final DatabaseReference mReactionsRef= FirebaseDatabase.getInstance().getReference().child("Reacciones");
+    private final DatabaseReference mReactionsRef= FirebaseDatabase.getInstance().getReference()
+            .child("Reacciones").child("id_proyectos");
+    private long nroMeGusta = 0;
+    private long nroMeEncanta = 0;
+    private long nroMeAsombra = 0;
 
 
 
@@ -89,9 +96,9 @@ public class SearchPlaceAdapter extends RecyclerView.Adapter<SearchPlaceAdapter.
     };
 
     @Override
-    public void onBindViewHolder(SearchPlaceAdapterViewHolder holder, final int position)
+    public void onBindViewHolder(final SearchPlaceAdapterViewHolder holder, final int position)
     {
-
+        final String uID = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         holder.txtPlace.setText(arrayListFiltered.get(position).getNombre());
         holder.cardPlace.setText(arrayListFiltered.get(position).getPais() +" - " +arrayListFiltered.get(position).getCiudad());
@@ -106,6 +113,45 @@ public class SearchPlaceAdapter extends RecyclerView.Adapter<SearchPlaceAdapter.
 
             }
         });
+        final String idProyecto = arrayListFiltered.get(position).getId();
+
+        mReactionsRef.child(idProyecto).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                nroMeGusta = dataSnapshot.child("me_gusta").getChildrenCount();
+                nroMeEncanta = dataSnapshot.child("me_encanta").getChildrenCount();
+                nroMeAsombra = dataSnapshot.child("me_asombra").getChildrenCount();
+                holder.meGustaImage.setVisibility(nroMeGusta>0?View.VISIBLE:View.GONE);
+                holder.meEncantaImage.setVisibility(nroMeEncanta>0?View.VISIBLE:View.GONE);
+                holder.meAsombraImage.setVisibility(nroMeAsombra>0?View.VISIBLE:View.GONE);
+                long totalReacciones = nroMeGusta + nroMeEncanta + nroMeAsombra;
+                if(totalReacciones>0)  {
+                    holder.nroReactions.setVisibility(View.VISIBLE);
+                    holder.nroReactions.setText(String.valueOf(totalReacciones));
+                }else {
+                    holder.nroReactions.setVisibility(View.GONE);
+                    holder.nroReactions.setText(" ");
+                }
+                holder.reactionImage.setImageResource(R.drawable.favorite_flaco);
+                holder.reactionImage.setTag("empty_reaction");
+                if (dataSnapshot.child("me_gusta").hasChild(uID)) {
+                    holder.reactionImage.setImageResource(R.drawable.ic_fb_like);
+                    holder.reactionImage.setTag("me_gusta");
+                }else if (dataSnapshot.child("me_encanta").hasChild(uID)) {
+                    holder.reactionImage.setImageResource(R.drawable.ic_fb_love);
+                    holder.reactionImage.setTag("me_encanta");
+                } else if (dataSnapshot.child("me_asombra").hasChild(uID)) {
+                    holder.reactionImage.setImageResource(R.drawable.ic_fb_wow);
+                    holder.reactionImage.setTag("me_asombra");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
         Log.e("EJEMPLO", "onReactionClick: ");
 
@@ -124,7 +170,7 @@ public class SearchPlaceAdapter extends RecyclerView.Adapter<SearchPlaceAdapter.
                             }
                         })
                         .withTextBackground(new ColorDrawable(Color.TRANSPARENT))
-                        .withTextColor(Color.GRAY)
+                        .withTextColor(Color.BLUE)
                         .withTextHorizontalPadding(0)
                         .withTextVerticalPadding(0)
                         .withTextSize(mCntx.getResources().getDimension(R.dimen.reactions_text_size))
@@ -136,36 +182,28 @@ public class SearchPlaceAdapter extends RecyclerView.Adapter<SearchPlaceAdapter.
                     }
                 });
 
-        final String uID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
         popup.setReactionSelectedListener(new Function1<Integer, Boolean>() {
             @Override
-            public Boolean invoke(Integer position) {
-                Log.i("Reactions", "Selection position ahora =" + position);
-                if (position != -1) {
-                    reactionsQuantity[position]++;
-                    Log.i("Reactions", strings[position] + " = " + reactionsQuantity[position]);
-                    // reacciones->
-                        //       idproyecto1->
-                            //          me_gusta ->
-                                        //          idEmprendedor1
-                                        //          idEmprendedor2
-                                        //          idEmprendedor3
-                            //          me_encanta ->
-                                                //          idEmprendedor1
-                                                //          idEmprendedor2
-                                                //          idEmprendedor3
-                        //       idproyecto2->
-                            //          me_gusta ->
-                                        //          idEmprendedor1
-                                        //          idEmprendedor2
-                                        //          idEmprendedor3
-                            //          me_encanta ->
-                                                //          idEmprendedor1
-                                                //          idEmprendedor2
-                                                //          idEmprendedor3
+            public Boolean invoke(Integer reactionPosition) {
+                if (reactionPosition != -1) {
 
+                    //holder.likeImage.getBackground().getAlpha() R.drawable.ic_fb_like
+                    String reaccion = (reaction_keys[reactionPosition]);
+                    String tagReaction = holder.reactionImage.getTag().toString();
+                    if(tagReaction.equalsIgnoreCase(reaccion)){
+                        mReactionsRef.child(idProyecto).child(reaccion).child(uID).removeValue();
+                    }else {
+                        mReactionsRef.child(idProyecto).child(reaccion).child(uID).setValue("id_emprendedor");
+                        for (int i = 0; i < reaction_keys.length; i++) {
+                            if(i!=reactionPosition){
+                                String reaccionDelete = (reaction_keys[i]);
+                                mReactionsRef.child(idProyecto).child(reaccionDelete).child(uID).removeValue();
+                            }
+                        }
+                    }
 
-                    //mReactionsRef.child(reaction_keys[position]).child(uID);
+                    notifyItemChanged(position);
                 }
 
                 // Close selector if not invalid item (testing purpose)
@@ -173,7 +211,7 @@ public class SearchPlaceAdapter extends RecyclerView.Adapter<SearchPlaceAdapter.
             }
         });
 
-        holder.likeImage.setOnTouchListener(popup);
+        holder.reactionImage.setOnTouchListener(popup);
     }
 
 
@@ -182,14 +220,22 @@ public class SearchPlaceAdapter extends RecyclerView.Adapter<SearchPlaceAdapter.
         TextView txtPlace;
         ImageView image;
         TextView cardPlace;
-        ImageView likeImage;
+        ImageView reactionImage; // txt_nro_reactions
+        ImageView meGustaImage; // txt_nro_reactions
+        ImageView meEncantaImage; // txt_nro_reactions
+        ImageView meAsombraImage; // txt_nro_reactions
+        TextView nroReactions;
 
         public SearchPlaceAdapterViewHolder(View itemView) {
             super(itemView);
             txtPlace = (TextView) itemView.findViewById(R.id.item_recycler_structure_title);
             image = (ImageView) itemView.findViewById(R.id.item_recycler_structure_imagen);
             cardPlace = (TextView) itemView.findViewById(R.id.item_recycler_structure_category);
-            likeImage = itemView.findViewById(R.id.like_icon);
+            reactionImage = itemView.findViewById(R.id.reaction_icon);
+            nroReactions = itemView.findViewById(R.id.txt_nro_reactions);
+            meGustaImage = itemView.findViewById(R.id.me_gusta_icon);
+            meEncantaImage = itemView.findViewById(R.id.me_encanta_icon);
+            meAsombraImage = itemView.findViewById(R.id.me_asombra_icon);
         }
     }
 
