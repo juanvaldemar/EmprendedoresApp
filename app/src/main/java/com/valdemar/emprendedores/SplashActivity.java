74a34310b44bf04f569e56e7da15fdf20b2d0b43
xmanu2015 <xmanu2015@gmail.com>
2021-03-16 10:48:39 -0500

    Corrección del flujo a abrir de nuevo el app y actualizacion de categorias en Registrar Empresa.

--- app/src/main/java/com/valdemar/emprendedores/SplashActivity.java
+++ app/src/main/java/com/valdemar/emprendedores/SplashActivity.java
@@ -8,14 +8,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.valdemar.emprendedores.auth.AccessRelato;
 import com.valdemar.emprendedores.auth.ViewSpook;
+import com.valdemar.emprendedores.view.ui.ClasificacionActivity;
 
 public class SplashActivity extends AppCompatActivity {
     private TextView mTitle;
@@ -40,18 +47,63 @@ public class SplashActivity extends AppCompatActivity {
         }*/
 
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if(user != null) {
-                    startActivity(new Intent(SplashActivity.this, MenuLateralActivity.class));
-                }else{
-                    startActivity(new Intent(SplashActivity.this, AccessRelato.class));
-                }
-                finish();
-            }
-        },400);
 
+
+        if(user != null) {
+            verificarRegistroTipoUsuario("Emprendedor", "id_emprendedor");
+            //startActivity(new Intent(SplashActivity.this, MenuLateralActivity.class));
+        }else{
+            startActivity(new Intent(SplashActivity.this, AccessRelato.class));
+        }
+        finish();
+
+    }
+
+    public void verificarRegistroTipoUsuario(final String nodo, final String idTipoUsuario){
+        final DatabaseReference mEmprendedorReference;
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        mEmprendedorReference = FirebaseDatabase.getInstance().getReference().child(nodo);
+        mEmprendedorReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot itemSpanshot: dataSnapshot.getChildren()) {
+                    String idUser = (String)itemSpanshot.child(idTipoUsuario).getValue();
+                    if(idUser!=null && idUser.equals(user.getUid())){
+                        redireccionarListaProyectos(nodo);
+                        return;
+                    }
+                }
+                if(nodo.equals("Empresa")){
+                    redireccionarMenuCondicional();
+                }
+                else
+                    verificarRegistroTipoUsuario("Empresa", "id_user");
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void redireccionarMenuCondicional(){
+        Intent i = new Intent(this, ClasificacionActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(i);
+        finish();
+    }
+
+    private void redireccionarListaProyectos(String opcion){
+        Intent i = new Intent(this, MenuLateralActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        i.putExtra("REQUIERE_REGISTRO", opcion);
+        i.putExtra("TIPO_ACCESO", opcion);
+        startActivity(i);
+        finish();
     }
 
 }
